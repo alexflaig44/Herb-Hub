07138317299b6868eb0cae352f2ed59ae356c5f0
alexflaig44 <alexflaig@wrAdmins-iMac-5.local>
2015-11-24 09:04:31 -0600

    11-24-2015
    9:04 AM

--- switchingscreens/app/src/main/java/student/mstc/switchingscreens/SecondScreen.java
+++ switchingscreens/app/src/main/java/student/mstc/switchingscreens/SecondScreen.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
  * Created by alexflaig on 11/17/15.
  */
 
-// A random line. 2nd Change.
+// A random line. 2nd Change. Test test
 
 public class SecondScreen extends Activity
 {
